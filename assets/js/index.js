a5246ed730ed3852543cee63cc62bbe4ea32c9b2
budistwn15 <budi.setiawan112001@gmail.com>
2021-11-11 09:38:34 +0700

    Add keyboard function

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,61 @@ const currentOperandTextElement = document.querySelector('.current-operand')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+const keyboard = document.getElementById("numb");
+keyboard.addEventListener("keypress", (event) => {
+  if(event.keyCode === 49){
+    console.log("1")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("one").click()
+  } else if(event.keyCode === 50){
+    console.log("2")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("two").click()
+  }else if(event.keyCode === 51){
+    console.log("3")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("three").click()
+  }else if(event.keyCode === 52){
+    console.log("4")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("four").click()
+  }else if(event.keyCode === 53){
+    console.log("5")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("five").click()
+  }else if(event.keyCode === 54){
+    console.log("6")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("six").click()
+  }else if(event.keyCode === 55){
+    console.log("7")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("seven").click()
+  }else if(event.keyCode === 56){
+    console.log("8")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("eight").click()
+  }else if(event.keyCode === 57){
+    console.log("9")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("nine").click()
+  }else if(event.keyCode === 48){
+    console.log("0")
+    event.preventDefault
+    calculator.updateDisplay()
+    document.getElementById("zero").click()
+  }
+})
+
 numbers.forEach(button => {
   button.addEventListener('click', () => {
     calculator.appendNumber(button.innerText)
---
+++ assets/js/keyboard.js
@@ -0,0 +1,19 @@
+const clickKeyboard = () => {
+        document.getElementById("one").disabled = false;
+        document.getElementById("two").disabled = false;
+        document.getElementById("three").disabled = false;
+        document.getElementById("four").disabled = false;
+        document.getElementById("five").disabled = false;
+        document.getElementById("six").disabled = false;
+        document.getElementById("seven").disabled = false;
+        document.getElementById("eight").disabled = false;
+        document.getElementById("nine").disabled = false;
+        document.getElementById("zero").disabled = false;
+        document.getElementById("dot").disabled = false;
+        document.getElementById("dis").disabled = false;
+        document.getElementById("multiply").disabled = false;
+        document.getElementById("sum").disabled = false;
+        document.getElementById("sub").disabled = false;
+        document.getElementById("mod").disabled = false;
+        document.getElementById("equal").disabled = false;
+}
